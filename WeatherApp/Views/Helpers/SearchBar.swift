fe356145022332193dca049477bad34a874a4129
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-04-10 00:56:46 +0300

    Add search bar.

--- WeatherApp/Views/Helpers/SearchBar.swift
+++ WeatherApp/Views/Helpers/SearchBar.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct SearchBar: View {
+    @EnvironmentObject var locationManager: LocationManager
+    let weatherManager = WeatherManager()
+    @State private var location: String = ""
+    @State private var isSearchActive = false
+    @Binding var currentWeather: Response
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isSearchActive {
+            TextField("Search", text: $location)
+        }
+        Button(action: {
+            isSearchActive.toggle()
+            Task {
+                weatherManager.getCoordinateFrom(address: location) { coordinate, error in
+                    guard let coordinate = coordinate else { return }
+                    Task {
+                        currentWeather = try await weatherManager.fetchWeather(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                        locationManager.updateCityName(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    }
+                }
+            }
+        }, label: {
+            Image(systemName: "magnifyingglass")
+        })
     }
 }
 
 struct SearchBar_Previews: PreviewProvider {
     static var previews: some View {
-        SearchBar()
+        SearchBar(currentWeather: .constant(weatherData))
     }
 }
